42c4fd5458d790b8a01d93114e17b220678b154d
Alex euzebio pereira dias de oliveira <127499015+Alexepddeoliveira@users.noreply.github.com>
2024-09-24 18:09:48 -0300

    Update main.py
    codigo main atualizado mqtt

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import cv2
 import time
 from hand_detection import detectar_maos, verificar_mao_aberta, getHandCoordinates
 from garra_control import atualizar_posicao_garra
-# from mqtt_client import enviar_dados_mqtt  # Preparado para a futura implementação MQTT
+from mqtt_client import enviar_dados_mqtt  # Importar a função de envio do MQTT
 
 # Variável de tempo para intervalos de iteração
-tempo = 0.3
+tempo = 2.0
 last_time = 0
 
 # Coordenada base e posição da garra
@@ -14,18 +14,6 @@ posicao_garra = [0, 0, 0, True]
 
 # Captura de vídeo (você pode alterar para webcam)
 cap = cv2.VideoCapture(0)
-
-def verificar_mao_esquerda_presente(result):
-    """
-    Função que verifica se a mão esquerda está presente.
-    Retorna True se a mão esquerda for detectada, caso contrário False.
-    """
-    if result.multi_handedness:
-        # Verifica se há alguma mão com o rótulo 'Left'
-        for handedness in result.multi_handedness:
-            if handedness.classification[0].label == 'Left':
-                return True
-    return False
 
 while True:
     success, img = cap.read()
@@ -51,11 +39,6 @@ while True:
     if current_time - last_time >= tempo:
         last_time = current_time
 
-        # Verifique se a mão esquerda está presente
-        if verificar_mao_esquerda_presente(result):
-            print("Captação Interrompida")
-            continue  # Pule para a próxima iteração sem processar a mão direita
-
         # Verifique se o resultado da detecção de mãos não é None
         if result.multi_handedness:
             # Obter as coordenadas da mão direita e verificar se está aberta
@@ -74,10 +57,10 @@ while True:
                     mao_aberta
                 ]
                 posicao_garra = atualizar_posicao_garra(relative_coordinates, posicao_garra)
-                print(f' X={posicao_garra[0]:.2f}, Y={posicao_garra[1]:.2f}, Z={posicao_garra[2]:.2f}, abrir garra: {posicao_garra[3]}')
+                print(f'Posição alvo da garra: X={posicao_garra[0]:.2f}, Y={posicao_garra[1]:.2f}, Z={posicao_garra[2]:.2f}, Mão aberta: {posicao_garra[3]}')
 
-                # Preparado para enviar dados via MQTT
-                # enviar_dados_mqtt(posicao_garra)
+                # Chamada contínua da função para enviar dados via MQTT
+                enviar_dados_mqtt(posicao_garra)
         else:
             print("Nenhuma mão detectada.")
 
